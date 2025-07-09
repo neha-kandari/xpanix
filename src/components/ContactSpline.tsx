9c6c9abb285c2e89952242bfc3093dcb6e653fcd
neha-kandari <kandarineha0508@gmail.com>
2025-07-09 17:35:42 +0530

    Update ContactSpline.tsx

--- src/components/ContactSpline.tsx
+++ src/components/ContactSpline.tsx
@@ -1,10 +1,1 @@
-"use client";
 
-import Spline from "@splinetool/react-spline";
-import React from "react";
-
-export default function ContactSpline() {
-  return (
-    <Spline scene="https://prod.spline.design/tJHwNqxPNSKu3Kfu/scene.splinecode" />
-  );
-} 
